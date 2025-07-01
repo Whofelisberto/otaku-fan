b0d417df788bfba03bba4f5f264a0a50516fee08
Whofelisberto <felisberto563@gmail.com>
2025-07-01 15:18:42 -0300

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
     <>
-    <div className="flex justify-center p-10 text-white text-sm bg-zinc-700">
-  <p>© OtakuFan - Todos os direitos reservados - <span>Feito por Whofelisberto</span></p>
-</div>
+      <div className="flex justify-center p-10 text-white text-sm bg-zinc-700">
+        <p>
+          © OtakuFan - Todos os direitos reservados -{" "}
+          <span>Feito por Whofelisberto</span>
+        </p>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
